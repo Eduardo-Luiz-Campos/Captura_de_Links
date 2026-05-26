7795a51c5c7d506555248dbcc9b386065b8f39ae
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-05-26 09:46:21 -0300

    finalizando lab1

--- lab1.js
+++ lab1.js
@@ -5,23 +5,38 @@ const URL_ALVO = 'https://books.toscrape.com';
 
 function buscarHTML(url) {
     return new Promise((resolve, reject) => {
-    // TODO 1: usar https.get para buscar a URL
-    // TODO 2: acumular os chunks em uma variável html
-    // TODO 3: ao finalizar (evento 'end', chamar resolve(html)
-    // TODO 4: em caso de erro, chamar reject(err)
+        //usar a variável 'url' por parâmetro
+        https.get(url, (res) => {
+            // TODO 2: acumular os chunks em uma variável html
+            let html = '';
+            res.on('data', chunk => html += chunk);
+
+            // TODO 3: ao finalizar (evento 'end', chamar resolve(html)
+            res.on('end', () => {
+                resolve(html);
+            });
+            
+        }).on('error', (err) => {
+            // TODO 4: em caso de erro, chamar reject(err)
+            reject(err);
+        });
     });
 }
 
 function extrairLinks(html) {
-    // TODO 5: criar uma Regex com a flag 'g' para capturar href=&quot;...&quot;
+    // TODO 5: criar uma Regex com a flag 'g' para capturar href="..."
+    const regex = /href="([^"]+)"/g;
+    
     // TODO 6: retornar um array com todas as URLs encontradas
+    const matches = [...html.matchAll(regex)];
+    return matches.map(match => match[1]);
 }
 
+// Execução do script
 buscarHTML(URL_ALVO)
     .then(html => {
         const links = extrairLinks(html);
         console.log(`Total de links encontrados: ${links.length}`);
         links.forEach(link => console.log(link));
-
-})
-.catch(err => console.error('Erro:', err.message));
+    })
+    .catch(err => console.error('Erro:', err.message));
